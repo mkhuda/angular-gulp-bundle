d1ff4304a475deda497d09bc3776b0083ab35ffc
mkhuda <mhmmd.huda@gmail.com>
2017-05-06 23:48:37 +0700

    Remove unused code

--- app/main.js
+++ app/main.js
@@ -5,12 +5,6 @@ require('angular-ui-router');
 var appku = angular.module('appku', ['ngRoute', 'ui.router']);
 
 appku.run(function($rootScope, $state, $location){
-  $rootScope.$on('$stateChangeStart', function(event, toState, toParams, fromState, fromParams){
-      console.log('stateChangeStart');
-      console.log('toState', toState);
-      console.log('fromState', fromState);
-      // $location.url('/about');
-      // console.log($state.get());
-  });
+  console.log("App started");
 });
 
