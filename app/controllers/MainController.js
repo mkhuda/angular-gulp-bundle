97cc637571522014b344d0dd875e119e76ca1441
mkhuda <mhmmd.huda@gmail.com>
2017-09-05 06:04:12 +0700

    rename appname

--- app/controllers/MainController.js
+++ app/controllers/MainController.js
@@ -1,6 +1,6 @@
 angular = require('angular');
 
-angular.module('appku')
+angular.module('angulargulpbundle')
   .controller('MainController', function ($scope, $state, $routeParams, $filter) {
     console.log('Main Controller Started');
     $scope.$state = $state;
