c984e1e8ea958ac0b7f2494cc5a60ef51cc13c89
mkhuda <mhmmd.huda@gmail.com>
2017-09-22 01:01:02 +0700

    other page one

---
+++ app/controllers/OtherTwoController.js
@@ -0,0 +1,6 @@
+angular = require('angular');
+
+angular.module('angulargulpbundler')
+  .controller('OtherController', function ($scope, $routeParams, $filter) {
+    console.log('Other Controller Started');
+  });
